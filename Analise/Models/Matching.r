b89e5d78c89951f8e1edcc02b72b625af39c192b
João Victor Nunes <joao.nunes@pickcells.bio>
2022-09-22 18:28:39 -0300

    evolving matching

--- Analise/Models/Matching.r
+++ Analise/Models/Matching.r
@@ -8,11 +8,19 @@ options(scipen = 100, digits=4)
 ## Adicionando diretório ao path
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-df <- read_parquet("../../Resultados/Agg/ENEM/aux/dados_para_matching.parquet") %>% select()
+df <- read_parquet("../../Resultados/Agg/ENEM/aux/dados_para_matching.parquet")
 
-colnames(df)
+# Selecionando apenas o ano de 2020 para fatorar a computação necessária
 
-matching <- matchit(treated ~ NU_ANO + TP_COR_RACA + Q005 + Q025 + TP_FAIXA_ETARIA + TP_ESCOLA + Q006 + IND_CASA + TP_SEXO + Q001 + Q022 + TP_ESTADO_CIVIL + NAO_BRANCO,
+df <- filter(df, NU_ANO == 2020)
+
+# Selecionando observações de estados nordestinos
+
+df <- filter(df, SG_UF_ESC %in% c("MA", "PI", "RN", "PB", "PE", "AL", "SE"))
+
+# Executando o matching com Coarsened Exact Matching
+
+matching <- matchit(Q025 ~ Q005 + Q025 + TP_FAIXA_ETARIA + TP_ESCOLA + Q006 + IND_CASA + MULHER + Q001 + Q022 + TP_ESTADO_CIVIL + NAO_BRANCO,
                     data = df,
                     method = 'cem', estimand = 'ATE'
                     )
@@ -21,3 +29,10 @@ summary(matching, un = FALSE)
 
 matched_df <- match.data(matching) %>% arrange(subclass, treated)
 
+# Performando análise de regressão
+model <- lm(NU_NOTA_MT ~ Q025 + MULHER + IND_CASA + TP_FAIXA_ETARIA + NAO_BRANCO + Q006, data = matched_df, weights = weights)
+coeftest(model, vcov. = vcovCL, cluster = ~subclass)
+
+coefci(model, vcov. = vcovCL, cluster = ~subclass)
+
+summary(model)
